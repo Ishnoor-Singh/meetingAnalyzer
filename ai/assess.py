e38ca835c460b25d400cb4ca5ec1ffdbedf0ea11
mrcolo <colo28197@gmail.com>
2020-11-19 21:56:25 -0800

    Added basic loop for presentation 2

--- ai/assess.py
+++ ai/assess.py
@@ -3,41 +3,65 @@ import cv2
 import tensorflow as tf
 
 face_detection = cv2.CascadeClassifier('haar_cascade_face_detection.xml')
-
-camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
-settings = {
-    'scaleFactor': 1.3, 
-    'minNeighbors': 5, 
-    'minSize': (50, 50)
-}
-
 labels = ["Neutral","Happy","Sad","Surprise","Angry"]
 
 model = tf.keras.models.load_model('expression.model')
 
-while True:
-	ret, img = camera.read()
+def classify_img(img):
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	detected = face_detection.detectMultiScale(gray, **settings)
-    
+	states = [[], [], [], [], []]
 	for x, y, w, h in detected:
 		cv2.rectangle(img, (x, y), (x+w, y+h), (245, 135, 66), 2)
 		cv2.rectangle(img, (x, y), (x+w//3, y+20), (245, 135, 66), -1)
 		face = gray[y+5:y+h-5, x+20:x+w-20]
 		face = cv2.resize(face, (48,48)) 
 		face = face/255.0
-		
 		predictions = model.predict(np.array([face.reshape((48,48,1))])).argmax()
 		state = labels[predictions]
-		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(img,state,(x+10,y+15), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
-		
-	cv2.imshow('Facial Expression', img)
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	if cv2.waitKey(5) != -1:
-		break
+		states[predictions].append(state)
+	return states
 
-camera.release()
-cv2.destroyAllWindows()
+def read_frames(video_path):
+	vidcap = cv2.VideoCapture(video_path)
+	success, image = vidcap.read()
+	ps_count = []
+	while success:
+		avg_p = np.array(
+			classify_img(image)).mean()
+		ps_count.append(avg_p)
+		_, image = vidcap.read()
+	return ps_count
+
+def parse_video(obj):
+	video_name, _id = obj
+	score = 0
+	with tempfile.TemporaryDirectory() as s3dir:
+		vp = os.path.join(s3dir, video_name)
+		s3.Bucket(BUCKET_NAME).download_file(
+			video_name, p)
+
+		# computes scores across meeting
+		scores = read_frames(vp)
+
+		# TODO compute score
+		score = -1
+	return score	
+
+def loop(db_collection, status_code, wait_time):
+    filtered = \
+        [(p['video_url'], p['_id'])
+            for p in db_collection.find() if p['status'] == status_code]
+
+    logging.debug(f'Received {filtered}')
+    for to_classify in filtered:
+		parse_video(to_classify)
+    logging.debug(f'Completed {filtered}')
+    time.sleep(wait_time)
+
+if __name__ == "__main__":
+	loop("our_collection", 0, 5)
+	camera.release()
+	cv2.destroyAllWindows()
+
+
