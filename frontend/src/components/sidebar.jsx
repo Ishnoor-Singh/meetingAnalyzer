7a199617cda5c351b2e692ebbc399ac788781986
Juan Carlo Magat <magatjuancarlo@gmail.com>
2020-11-21 16:28:13 -0800

    Improve the sidebar
    Also add debug.log to .gitignore

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,28 +1,67 @@
-import React, { Component } from "react";
-import { Divider, Drawer, IconButton } from "@material-ui/core";
-import { AccountCircle, AddCircle, Dashboard } from "@material-ui/icons";
+import React from "react";
+import {
+  Button,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import { CloudUpload, Dashboard } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
 
-class Sidebar extends Component {
-  state = {};
+const drawerWidth = 208;
 
-  render() {
-    return (
-      <Drawer variant="permanent">
-        <IconButton>
-          <AccountCircle />
-        </IconButton>
-        <Divider />
-        <IconButton>
-          <Dashboard />
-        </IconButton>
-        <Divider />
-        <IconButton>
-          <AddCircle />
-        </IconButton>
-        <Divider />
-      </Drawer>
-    );
-  }
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: {
+    position: "relative",
+    top: 16,
+  },
+  upload: {
+    margin: theme.spacing(1),
+    position: "relative",
+    top: 616,
+  },
+}));
+
+export default function Sidebar(props) {
+  const classes = useStyles(props);
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{ paper: classes.drawerPaper }}
+      anchor="left"
+    >
+      <h1>Viber</h1>
+      <Divider />
+
+      <List className={classes.toolbar}>
+        <ListItem button>
+          <ListItemIcon>
+            <Dashboard />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+      </List>
+
+      <Button
+        className={classes.upload}
+        variant="contained"
+        color="primary"
+        startIcon={<CloudUpload />}
+      >
+        UPLOAD
+      </Button>
+    </Drawer>
+  );
 }
-
-export default Sidebar;
