e96b9b2ec749b74d82441f1fd556a1a63fd2f2ac
Juan Carlo Magat <magatjuancarlo@gmail.com>
2020-12-01 11:15:01 -0800

    Revamp the upload page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/dashboard";
-import Upload from "./components/upload";
+import DropzoneDialog from "./components/upload";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
-      <Upload></Upload>
+      <DropzoneDialog></DropzoneDialog>
     </div>
   );
 }
