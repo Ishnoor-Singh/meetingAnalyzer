b85636b5736992a6f7753fa63b573d0d75ed28c5
Ashkan Faghihi <ashkan117@yahoo.com>
2020-12-02 21:04:45 -0800

    Passing 100 for reports, cleaned up code

--- backend/src/controllers/report.controller.js
+++ backend/src/controllers/report.controller.js
@@ -2,19 +2,6 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { dbService } = require('../services');
 
-const addReport = catchAsync(async (req, res) => {
-  // TODO: Add the s3 bucket info
-  // creates empty document
-  const id = await dbService.addReport(req.body);
-  res.send(id)
-});
-
-const updateReport = catchAsync(async (req, res) => {
-  const report = await dbService.updateReport(req.params.reportId,req.body);
-  // res.status(httpStatus.NO_CONTENT).send();
-  res.send(report)
-});
-
 const searchReport = catchAsync(async (req, res) => {
   const report = await dbService.searchReport(req.params.reportId);
   if (report == null){
@@ -29,14 +16,14 @@ const searchReport = catchAsync(async (req, res) => {
     status = 200
   }
   // TODO: remove
-  await dbService.updateReport(req.params.reportId, {labels:['5', '10', '15'], data:[Math.random()*15, Math.random()*100, Math.random()*50], status: 1})
+  // await dbService.updateReport(req.params.reportId, {labels:['5', '10', '15'], data:[Math.random()*15, Math.random()*100, Math.random()*50], status: 1})
 
   res.status(status).send({report, msg: status === 200?'Report Ready':'Processing Video'})
 
 });
 
 module.exports = {
-  addReport,
-  updateReport,
+  // addReport,
+  // updateReport,
   searchReport
 };
