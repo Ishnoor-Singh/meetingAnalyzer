322b1126c903d1f61e1ea6f5f4493a0ed6341f3d
francesco <fcolonnese@ucla.edu>
2020-12-04 11:32:49 -0800

    fix

--- ai/test.py
+++ ai/test.py
@@ -2,7 +2,7 @@ import unittest
 import cv2
 from assess import read_frames, compute_score, classify_img
 import numpy
-class TestViber(unittest.TestCase):
+class TestViberAI(unittest.TestCase):
     def test_score(self):
         self.assertTrue(isinstance(compute_score(read_frames("./short.mp4")), float))
     def test_classify(self):
